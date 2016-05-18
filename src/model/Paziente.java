d3924ed2573c60a2e70a1b8bb0b24266e955e2ef
simone <petrux91@gmail.com>
2016-05-18 14:59:04 +0200

    primo commit

---
+++ src/model/Paziente.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Paziente {
+
+	private String nome, cognome, id;
+	
+	public Paziente(String nome, String cognome) {
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+}
