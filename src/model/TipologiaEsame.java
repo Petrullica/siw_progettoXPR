eabe37cfe0d6e1535308be526af23704528dd8ef
simone <petrux91@gmail.com>
2016-05-19 17:50:20 +0200

    ma che ne so sto cacchio de git fa casini

--- src/model/TipologiaEsame.java
+++ src/model/TipologiaEsame.java
@@ -7,36 +7,66 @@ public class TipologiaEsame {
 	private Long id;
 	private String nome;
 	private String descr;
+	private double price;
 	private Map<String,String> prerequisti;
 	private Map<String,String> indicatoriRequisiti;
 	
+	public TipologiaEsame(String nome, String descr, double price) {
+		this.nome = nome;
+		this.descr = descr;
+		this.price = price;
+	}
+	
+	
+	
+	public TipologiaEsame(String nome, String descr, double price, Map<String, String> prerequisti,
+			Map<String, String> indicatoriRequisiti) {
+		this.nome = nome;
+		this.descr = descr;
+		this.price = price;
+		this.prerequisti = prerequisti;
+		this.indicatoriRequisiti = indicatoriRequisiti;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	public Long getId() {
 		return id;
 	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getDescr() {
 		return descr;
 	}
+	
 	public void setDescr(String descr) {
 		this.descr = descr;
 	}
+	
 	public Map<String, String> getPrerequisti() {
 		return prerequisti;
 	}
+	
 	public void setPrerequisti(Map<String, String> prerequisti) {
 		this.prerequisti = prerequisti;
 	}
+	
 	public Map<String, String> getIndicatoriRequisiti() {
 		return indicatoriRequisiti;
 	}
+	
 	public void setIndicatoriRequisiti(Map<String, String> indicatoriRequisiti) {
 		this.indicatoriRequisiti = indicatoriRequisiti;
 	}
