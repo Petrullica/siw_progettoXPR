0b5a84bd4ff380cf49f01536ee571cbb4f65dc2c
TengXu94 <Last@10.25.136.135>
2016-06-01 10:58:04 +0200

    sono io Last

--- src/action/Action.java
+++ src/action/Action.java
@@ -2,7 +2,7 @@ package action;
 
 import model.Facade;
 import javax.servlet.http.HttpServletRequest;
-
+//lol
 public interface Action {
 
 	public String execute(HttpServletRequest request, Facade facade);
