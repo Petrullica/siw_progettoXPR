f13369b202a7ac273b38e8c844971c068f09b57e
Alessandro <alessandro.raguzzi@yahoo.it>
2016-06-02 22:43:31 +0200

    Ho lavorato sulle classi EsameController e EsameFacade, ma non sono complete

--- src/controller/EsameController.java
+++ src/controller/EsameController.java
@@ -11,6 +11,9 @@ import facade.EsameFacade;
 import model.Esame;
 import model.TipologiaEsame;
 
+
+//DA SISTEMARE LA RISPETTIVA FACADE
+
 @SessionScoped
 @ManagedBean
 public class EsameController {
@@ -20,15 +23,66 @@ public class EsameController {
 	private String nome;
 	private Esame esame;
 	private Date dataPrenotazioneEsame;
-	private TipologiaEsame tipologiaEsame;
+	private Date dataSvolgimentoEsame;
 	
 	@EJB
 	
 	private EsameFacade esameFacade;
 	
-    public String creaEsame(){
-    	this.esame= esameFacade.creaEsame(codice, nome, dataPrenotazioneEsame, tipologiaEsame);
+	public String creaEsame(){
+    	this.esame= esameFacade.creaEsame(codice, nome, dataPrenotazioneEsame,dataSvolgimentoEsame);
 		return "esame";
     	
     }
+	
+	public String mostraEsami(){
+		this.esame= esameFacade.getAllEsami();
+		return "esame";
+	}
+	
+	public String findEsame() {
+		this.esame = esameFacade.getPaziente(this.codice);
+		return "esame";
+	}
+	
+    public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Esame getEsame() {
+		return esame;
+	}
+
+	public void setEsame(Esame esame) {
+		this.esame = esame;
+	}
+
+	public Date getDataPrenotazioneEsame() {
+		return dataPrenotazioneEsame;
+	}
+
+	public void setDataPrenotazioneEsame(Date dataPrenotazioneEsame) {
+		this.dataPrenotazioneEsame = dataPrenotazioneEsame;
+	}
+
+	public EsameFacade getEsameFacade() {
+		return esameFacade;
+	}
+
+	public void setEsameFacade(EsameFacade esameFacade) {
+		this.esameFacade = esameFacade;
+	}
+
 }
--- src/facade/EsameFacade.java
+++ src/facade/EsameFacade.java
@@ -2,15 +2,40 @@ package facade;
 
 import java.util.Date;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import model.Esame;
+import model.Paziente;
 import model.TipologiaEsame;
 
+
+//DA IMPLEMENTARE, ho vari dubbi su Esame, ad esempio siamo sicuri sia giusto il costruttore?
+
+@Stateless
 public class EsameFacade {
 
-	public Esame creaEsame(String codice, String nome, Date dataPrenotazioneEsame, TipologiaEsame tipologiaEsame) {
+	@PersistenceContext(unitName="clinica-unit")
+    private EntityManager em;
+ 
+	
+	public Esame creaEsame(String codice, String nome, Date dataPrenotazioneEsame, Date dataSvolgimentoEsame) {
+		//Esame esame = new Esame(codice, nome, dataPrenotazioneEsame, dataSvolgimentoEsame);
+		//em.persist(esame);
+		return null;
+	}
+
+	public Esame getAllEsami() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Esame getPaziente(String codice) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 }
+
 
