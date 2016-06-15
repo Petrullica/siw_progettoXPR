cf1035a4fd91534f073a953638e56381d153583d
simone <petrux91@gmail.com>
2016-06-15 10:44:08 +0200

    tolte cose inutili

--- src/model/TipologiaEsame.java
+++ src/model/TipologiaEsame.java
@@ -1,24 +1,15 @@
 package model;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.UniqueConstraint;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.PrimaryKeyJoinColumns;
 
 @Entity
 public class TipologiaEsame {
